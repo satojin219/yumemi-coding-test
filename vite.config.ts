a0b1e7f9bcedd524e2d60a300f8953fd130ff8c5
satojin <renzuoteng142@gmail.com>
2022-11-20 10:42:38 +0900

    chore: storybookにemotionを適応

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      jsxImportSource: '@emotion/react'
+    })
+  ],
   test: {
     globals: true,
     environment: 'happy-dom'
