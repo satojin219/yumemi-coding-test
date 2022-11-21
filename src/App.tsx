3c739f7529feba927af8db9c0c7354cb5557f3ea
satojin <renzuoteng142@gmail.com>
2022-11-21 22:18:00 +0900

    feat/reafactorUI

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,16 @@ import noramlize from 'normalize.css'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { Home } from './Home'
 
-const styles = css`
+const globalStyle = css`
   ${noramlize}
 `
+
 
 function App(): ReactElement {
   const queryClient = new QueryClient()
   return (
     <QueryClientProvider client={queryClient}>
-      <Global styles={styles} />
+      <Global styles={globalStyle} />
       <Home />
     </QueryClientProvider>
   )
--- src/Home.tsx
+++ src/Home.tsx
@@ -1,8 +1,20 @@
+import { css } from '@emotion/react'
 import { HighChartGraph } from './components/HighChartGraph'
 import { PrefectureList } from './components/PrefectureList'
 import { useCheckedPopulation } from './hooks/useCheckedPopulation'
 import { getPopulationComposition } from './hooks/useFetchPopulationComposition'
 
+const container = css`
+  margin: 0 10%;
+`
+const title = css`
+  text-align: center;
+`
+const label = css`
+  padding: 0.5em 1em;
+  margin: 1em auto;
+  border-left: 5px solid;
+`
 export const Home: React.FC = () => {
   const { categories, series, addPrefecture, removePrefecture } =
     useCheckedPopulation()
@@ -20,10 +32,12 @@ export const Home: React.FC = () => {
   }
 
   return (
-    <>
-      <h1>ゆめみ フロントエンドコーディング試験</h1>
+    <div css={container}>
+      <h1 css={title}>ゆめみ フロントエンドコーディング試験</h1>
+      <h2 css={label}>都道府県一覧</h2>
       <PrefectureList onChange={handleClickCheck} />
+      <h2 css={label}>人口推移グラフ</h2>
       <HighChartGraph categories={categories} series={series} />
-    </>
+    </div>
   )
 }
