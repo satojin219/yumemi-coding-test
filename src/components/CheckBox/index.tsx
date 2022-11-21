ce5bfab8f6847d10f5902ce3d9fc42aebcab2819
satojin <renzuoteng142@gmail.com>
2022-11-21 22:41:48 +0900

    style: checkboxコンポーネントのスタイル調整

--- src/components/CheckBox/index.tsx
+++ src/components/CheckBox/index.tsx
@@ -4,12 +4,22 @@ import { css } from '@emotion/react'
 type Props = {
   label: string
   value: number
-  onChange: (prefCode: number,prefName: string, isCheck: boolean) => void
+  onChange: (prefCode: number, prefName: string, isCheck: boolean) => void
 }
 
 export const CheckBox: React.FC<Props> = (props) => {
   const container = css`
     margin: 0.3em 1em;
+  `
+  const input = css`
+    cursor: pointer;
+    :checked + label {
+      color: #0075ff;
+    }
+  `
+
+  const label = css`
+    margin-left: 0.5em;
   `
 
   return (
@@ -19,11 +29,13 @@ export const CheckBox: React.FC<Props> = (props) => {
         value={props.value}
         id={`check_box_${props.value}`}
         onClick={(event) => {
-          props.onChange(props.value,props.label, event.currentTarget?.checked)
-        }
-        }
+          props.onChange(props.value, props.label, event.currentTarget?.checked)
+        }}
+        css={input}
       />
-      <label htmlFor="">{props.label}</label>
+      <label htmlFor={`check_box${props.value}`} css={label}>
+        {props.label}
+      </label>
     </div>
   )
 }
